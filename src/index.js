6a26833d7302a2da5eba1f11fa1b2ff6341bb7e7
Mukund Manikarnike <mukunm@gmail.com>
2021-12-11 15:25:39 -0500

    Some pending commits. Not sure what this is.

--- src/index.js
+++ src/index.js
@@ -1,64 +1,44 @@
-import React, { Component } from 'react';
-import VideoPlayer from 'react-video-js-player';
- 
-class VideoApp extends Component {
-    player = {}
-    state = {
-        video: {
-            src: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-            _poster: "http://www.example.com/path/to/video_poster.jpg"
-        }
+import React from 'react';
+import videojs from 'video.js'
+import ReactDOM from 'react-dom';
+import 'video.js/dist/video-js.css'
+
+export default class VideoPlayer extends React.Component {
+  componentDidMount() {
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log('onPlayerReady', this)
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
     }
- 
-    onPlayerReady(player){
-        console.log("Player is ready: ", player);
-        this.player = player;
-    }
- 
-    onVideoPlay(duration){
-        console.log("Video played at: ", duration);
-    }
- 
-    onVideoPause(duration){
-        console.log("Video paused at: ", duration);
-    }
- 
-    onVideoTimeUpdate(duration){
-        console.log("Time updated: ", duration);
-    }
- 
-    onVideoSeeking(duration){
-        console.log("Video seeking: ", duration);
-    }
- 
-    onVideoSeeked(from, to){
-        console.log(`Video seeked from ${from} to ${to}`);
-    }
- 
-    onVideoEnd(){
-        console.log("Video ended");
-    }
- 
-    render() {
-        return (
-            <div>
-                <VideoPlayer
-                    controls={true}
-                    src={this.state.video.src}
-                    poster={this.state.video.poster}
-                    width="720"
-                    height="420"
-                    onReady={this.onPlayerReady.bind(this)}
-                    onPlay={this.onVideoPlay.bind(this)}
-                    onPause={this.onVideoPause.bind(this)}
-                    onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
-                    onSeeking={this.onVideoSeeking.bind(this)}
-                    onSeeked={this.onVideoSeeked.bind(this)}
-                    onEnd={this.onVideoEnd.bind(this)}
-                />
-            </div>
-        );
-    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div>	
+        <div data-vjs-player>
+          <video ref={ node => this.videoNode = node } className="video-js"></video>
+        </div>
+      </div>
+    )
+  }
 }
-ReactDOM.render(<VideoApp />, document.getElementById("root"));
-//export default VideoApp;
+
+const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [{
+      src: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+      type: 'application/x-mpegURL'
+    }]
+  }
+
+  ReactDOM.render(<VideoPlayer { ...videoJsOptions } />, document.getElementById("root"));
